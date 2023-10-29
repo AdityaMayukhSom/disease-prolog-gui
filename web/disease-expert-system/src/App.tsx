f340a40c75a086f15ed0493e75ea0337742ba472
adityamayukhsom <adi.kg2@gmail.com>
2023-10-29 14:16:03 +0530

    added error, fixed UI

--- web/disease-expert-system/src/App.tsx
+++ web/disease-expert-system/src/App.tsx
@@ -2,13 +2,12 @@ import { ChangeEvent, useEffect, useState } from "react";
 
 import Question from "./Question";
 import Diseases from "./Diseases";
+import Loading from "./Loading";
 import {
   getDiseases,
   getInitialQuestions,
   getNextQuestions,
 } from "../data/APICalls";
-
-import "./App.css";
 
 const f = (x: number) => (1 / 4) * (x * x - x);
 const g = (x: number) => (1 / 2) * (x * x - 2 * x + 2);
@@ -22,19 +21,42 @@ enum Steps {
 
 function App() {
   const [step, setStep] = useState<Steps>(Steps.INITIAL_INQUIRY);
+  const [error, setError] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(true);
-  const [questions, setQuestions] = useState<string[]>([]);
+  const [questions, setQuestions] = useState<string[][]>([]);
   const [alreadyAskedMask, setAlreadyAskedMask] = useState<bigint>(BigInt(0n));
   const [selectedMask, setSelectedMask] = useState<bigint>(BigInt(0n));
   const [selectedSymptoms, setSelectedSymptoms] = useState<string[]>([]);
   const [diseases, setDiseases] = useState<string[]>([]);
+
+  const addOrRemoveSymptom = async (e: ChangeEvent<HTMLInputElement>) => {
+    setError(false);
+    if (selectedSymptoms.includes(e.target.value)) {
+      setSelectedSymptoms((prev) =>
+        prev.filter((symptom) => symptom !== e.target.value)
+      );
+    } else {
+      setSelectedSymptoms((prev) => [...prev, e.target.value]);
+    }
+  };
+
+  const renderInitialQuestions = async () => {
+    const { initial_questions, selected_mask, already_asked_mask } =
+      await getInitialQuestions();
+
+    setQuestions(initial_questions);
+    setSelectedMask(selected_mask);
+    setAlreadyAskedMask(already_asked_mask);
+    setLoading(false);
+  };
 
   const nextStep = async () => {
     setLoading(true);
 
     if (step === Steps.INITIAL_INQUIRY) {
       if (selectedSymptoms.length === 0) {
-        alert("must select atleast one symptom to continue...");
+        // alert("must select atleast one symptom to continue...");
+        setError(true);
       } else {
         const { next_questions, selected_mask, already_asked_mask } =
           await getNextQuestions(
@@ -46,6 +68,7 @@ function App() {
         if (next_questions.length === 0) {
           setStep(Steps.DISEASE_DIAGNOSIS);
           setDiseases([]);
+          setLoading(false);
           return;
         }
 
@@ -70,61 +93,48 @@ function App() {
     setLoading(false);
   };
 
-  const addOrRemoveSymptom = async (e: ChangeEvent<HTMLInputElement>) => {
-    if (selectedSymptoms.includes(e.target.value)) {
-      setSelectedSymptoms((prev) =>
-        prev.filter((symptom) => symptom !== e.target.value)
-      );
-    } else {
-      setSelectedSymptoms((prev) => [...prev, e.target.value]);
-    }
-  };
-
   useEffect(() => {
-    (async () => {
-      const { initial_questions, selected_mask, already_asked_mask } =
-        await getInitialQuestions();
-
-      setQuestions(initial_questions);
-      setSelectedMask(selected_mask);
-      setAlreadyAskedMask(already_asked_mask);
-      setLoading(false);
-    })();
+    // setTimeout(() => {
+    renderInitialQuestions();
+    // }, 20000);
   }, []);
 
   if (loading) {
-    return (
-      <>
-        <h1>disease expert system.</h1>
-        <div className="card">Loading...</div>
-      </>
-    );
+    return <Loading />;
   }
 
   return (
-    <>
-      <h1>disease expert system.</h1>
+    <section className="w-full flex justify-center items-center flex-col">
+      <h1 className=" text-4xl py-4">disease expert system.</h1>
       {step === 3 ? (
         <Diseases diseases={diseases} />
       ) : (
         questions.length && (
-          <form className="card">
+          <form className=" w-full items-start max-w-2xl">
             {questions
               .slice(f(step) * questions.length, g(step) * questions.length)
               .map((question) => (
                 <Question
-                  key={question}
+                  key={question[0]}
                   question={question}
                   addOrRemoveSymptom={addOrRemoveSymptom}
                 />
               ))}
+
+            {error && (
+              <h4 className=" text-red-400 text-sm py-3">
+                You must select atleast one symptom to proceed further...
+              </h4>
+            )}
             <button type="button" onClick={nextStep}>
-              Continue
+              {step === Steps.SECOND_DETAILED_INQUIRY
+                ? "Find Disease"
+                : "Continue To Next Step"}
             </button>
           </form>
         )
       )}
-    </>
+    </section>
   );
 }
 
